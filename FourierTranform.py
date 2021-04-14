a1a6de068b0a7c181ee10d1b63607c1a7cb4b011
m4rr3co <m4rr3co@hotmail.com>
2021-04-14 17:06:47 -0300

    Amplitude of the fast fourier transform needs work.

--- FourierTranform.py
+++ FourierTranform.py
@@ -9,30 +9,35 @@ import numpy as np
 import matplotlib.pyplot as plot
 from scipy.fft import fft, fftfreq
 
-# Free Vibration of a Single-Degree of Freedom Mass-Spring-Dampener System
-# Input: Impulse
-# Output: x(t) = (e^(zeta*wn*t)/m*wd)*sin(wd*t)
-# Parameters
-j = 0+1j  # Imaginary unit
-mass = m = 1  # Kilograms [kg]
-stiffness = k = 10e3  # Newton/meter [N/m]
-damping_ratio = [0.1,0.01,0.001]
-natural_frequency = wn = np.sqrt(stiffness/mass)
-fn = wn / (2 * np.pi)
-critical_damping = cc = 2*m*wn
-c = cc * damping_ratio[0]
-wd = wn*np.sqrt(1-(damping_ratio[0]**2))
-fd = wd / (2 * np.pi)
+# Sampling Parameters
+sample_frequency = 12893  # Hz
+window = 1252.58204  # seconds
+start_time = 5.12837  # seconds
+end_time = start_time+window  # seconds
+number_of_samples = N = int(sample_frequency * window)  # seconds
 
-angular_velocity = w = np.arange(0,300,1)
-H_wj = 1 / (k-(m*w**2)+(w*c*j))
+# Signal: Impulse Response Function
+mass = m = 1  # [Kg]
+stiffness = k = 10e3  # [N/m]
+wn = np.sqrt(k/m)  # [rad/s] - Natural Frequency
+fn = wn / (2 * np.pi)  # [Hz] - Natural Frequency
+damping_ratio = 0.01  # Zeta
+critical_damping = cc = 2*np.sqrt(k*m)  # [Ns/m]
+c = damping_ratio * critical_damping  # [Ns/m] - Damping
+wd = wn * np.sqrt(1-damping_ratio**2)  # [rad/s] - Damped Natural Frequency
+fd = wd / (2 * np.pi)  # [Hz] - Damped Natural Frequency
+print('Calculated Damped Natural Frequency = '+str(fd)+' Hz.')
 
-sample_spacing = dt = 0.001
-window = 100
-time_interval = t = np.arange(0,window,dt)
-x = (np.exp(-damping_ratio[0]*(2 * np.pi * fn)*t)/(2 * np.pi * fd)*m)*np.sin((2 * np.pi * fd)*t)
+time_interval = t = np.linspace(start_time,end_time,number_of_samples)
+samples = y = (np.exp(-damping_ratio*(2 * np.pi * fn)*t)/(2 * np.pi * fd)*m)*np.sin(2 * np.pi * fd * t)
 
-H_dwj = fft(x)
-freq_bins = np.linspace(0,window * 10,len(H_dwj))
-plot.plot(fftfreq(len(H_dwj),dt),np.abs(H_dwj))
+ft = fft(y)  # Fast Fourier Transform of the sampled signal
+
+#  Normalized frequency bins.   (i.e.: The X axis represents the frequency components
+#                               of the sampled signals in Hz.)
+freq_bins = np.linspace(0,sample_frequency,number_of_samples)
+plot.plot(freq_bins,np.abs(ft))  # fft X frequency bins chart
 plot.show()
+
+max_index = np.where(np.abs(ft) == max(np.abs(ft)))
+print('Damped Natural Frequency from FFT: '+str(freq_bins[max_index])+' Hz.')
