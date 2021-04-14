1a02eac25587ce62f4df7d95e1393664430b924c
m4rr3co <m4rr3co@hotmail.com>
2021-04-13 21:34:37 -0300

    Working on this.

--- FourierTranform.py
+++ FourierTranform.py
@@ -18,7 +18,7 @@ mass = m = 1  # Kilograms [kg]
 stiffness = k = 10e3  # Newton/meter [N/m]
 damping_ratio = [0.1,0.01,0.001]
 natural_frequency = wn = np.sqrt(stiffness/mass)
-fn = wn/ (2 * np.pi)
+fn = wn / (2 * np.pi)
 critical_damping = cc = 2*m*wn
 c = cc * damping_ratio[0]
 wd = wn*np.sqrt(1-(damping_ratio[0]**2))
@@ -28,10 +28,11 @@ angular_velocity = w = np.arange(0,300,1)
 H_wj = 1 / (k-(m*w**2)+(w*c*j))
 
 sample_spacing = dt = 0.001
-window = 2 * np.pi * 100
+window = 100
 time_interval = t = np.arange(0,window,dt)
 x = (np.exp(-damping_ratio[0]*(2 * np.pi * fn)*t)/(2 * np.pi * fd)*m)*np.sin((2 * np.pi * fd)*t)
 
 H_dwj = fft(x)
-plot.plot(np.abs(H_dwj))
+freq_bins = np.linspace(0,window * 10,len(H_dwj))
+plot.plot(fftfreq(len(H_dwj),dt),np.abs(H_dwj))
 plot.show()
