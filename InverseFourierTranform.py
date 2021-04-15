dfffd3cf097a7162ee3f5a09b688123046537c53
m4rr3co <m4rr3co@hotmail.com>
2021-04-15 20:05:14 -0300

    Working on this.

--- InverseFourierTranform.py
+++ InverseFourierTranform.py
@@ -9,13 +9,6 @@ import numpy as np
 import matplotlib.pyplot as plot
 from scipy.fft import ifft
 
-# Sampling Parameters
-sample_frequency = 100  # Hz
-window = 10  # seconds
-# start_time = 0  # seconds
-# end_time = start_time+window  # seconds
-number_of_samples = N = int(sample_frequency * window)  # seconds
-
 # Signal: Impulse Response Function
 mass = m = 1  # [Kg]
 stiffness = k = 10e3  # [N/m]
@@ -28,9 +21,17 @@ wd = wn * np.sqrt(1-damping_ratio**2)  # [rad/s] - Damped Natural Frequency
 fd = wd / (2 * np.pi)  # [Hz] - Damped Natural Frequency
 print('Calculated Damped Natural Frequency = '+str(fd)+' Hz.')
 
-#  freq_bins: The X axis represents the frequency components of the sampled signals in Hz.
-freq_bins = f = np.linspace(-sample_frequency,sample_frequency,number_of_samples)
-frf = 1/(k-(m*((2 * np.pi * f)**2))+(1j * c * (2 * np.pi * f)))
+# Sampling Parameters
+frequency_range = 35
+sample_spacing = 0.001
+number_of_samples = int(frequency_range/sample_spacing)
 
-plot.plot(f,np.abs(frf))
+#  freq_bins: The X axis represents the frequency components of the sampled signals in Hz.
+freq_bins = f = np.linspace(-frequency_range/2,frequency_range/2,number_of_samples)
+frf = 1/(k-(m*((2 * np.pi * f)**2))+(1j * c * (2 * np.pi * f)))
+ift = ifft(frf)
+
+
+# plot.plot(np.abs(frf))
+plot.plot(np.real(ift)[:200])
 plot.show()
