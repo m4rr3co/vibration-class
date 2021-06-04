1f9edda5106bc4a2190b4e85623d208fd5e74258
m4rr3co <m4rr3co@hotmail.com>
2021-06-04 10:41:18 -0300

    Working on this: Needs plots.

--- FRFEstimation.py
+++ FRFEstimation.py
@@ -9,69 +9,62 @@ import numpy as np
 import matplotlib.pyplot as plot
 from scipy import signal
 from scipy.fft import fft
+from scipy.signal import coherence as ch
 
 # System parameters
 mass = m = 1  # Kilograms [kg]
 stiffness = k = 1e4  # Newton/meter [N/m]
 wn = np.sqrt(stiffness/mass)  # Natural frequency
 critical_damping = cc = 2*m*wn  # zeta = c/cc
-zeta = 0.001
-c = zeta*cc
-wd = wn*np.sqrt(1-(zeta ** 2))
+zeta = [0.1,0.01,0.001]
+c = [0,0,0]
+for i in range(len(zeta)):
+    c[i] = zeta[i]*cc
+wd = [0,0,0]
+for i in range(len(zeta)):
+    wd[i] = wn*np.sqrt(1-(zeta[i] ** 2))
 
 # Sampling parameters
-sampling_interval = delta = 0.079
+sampling_interval = delta = 0.001
 sampling_frequency = fs = 1/delta
-tmax = 133
+tmax = 5
 t = np.arange(0,tmax,delta)
 
-# Input Force
-A = 100  # [N]
-f_t = A*(np.random.rand(len(t))-0.5)
-h = (np.exp(-zeta*wn*t)/wd*m)*np.sin(wd*t)
-x_t = np.convolve(h,f_t,'full')
-x_t_max = max(x_t)
-x_t_noised = x_t[:len(t)] + 0.2*x_t_max*(np.random.rand(len(t))-0.5)
+for i in range(len(zeta)):
+    # Input Force
+    f_t = np.random.normal(0,1,len(t))
+    h = (np.exp(-zeta[i]*wn*t)/wd[i]*m)*np.sin(wd[i]*t)
+    x_t = np.convolve(h,f_t,'full')[:len(t)]
+    x_t = x_t + np.random.normal(0,0.00005,len(t))
+    noise = max(x_t)
 
-# Fourier Transforms
-Xw = fft(x_t[:len(t)])*delta
-Xw_noised = fft(x_t_noised)*delta
-Fw = fft(f_t)*delta
-Xw_double_sided = np.concatenate((np.flip(np.conj(Xw[:len(Xw)//2])),Xw[:len(Xw)//2+1]))
-w = np.linspace(0,fs,len(t))
-w_double_sided = np.linspace(-fs/2,fs/2,len(t))
+    # Fourier Transforms
+    freq_bins = f = np.linspace(0,fs,len(t))
+    Xw = fft(x_t)*delta
+    Fw = fft(f_t)*delta
 
+    # Power Spectral Densities
+    Sfx = (np.conj(Fw)*Xw)/tmax
+    Sff = (np.conj(Fw)*Fw)/tmax
+    Sxx = (np.conj(Xw)*Xw)/tmax
 
-# Power Spectral Densities
-Sfx = (np.conj(Fw)*Xw)/tmax
-Sff = (np.conj(Fw)*Fw)/tmax
-Sxx = (np.conj(Xw)*Xw)/tmax
+    H1 = Sfx/Sff
 
-Sfx_noised = (np.conj(Fw)*Xw_noised)/tmax
-Sff_noised = (np.conj(Fw)*Fw)/tmax
-Sxx_noised = (np.conj(Xw_noised)*Xw_noised)/tmax
+    f1,cohe = ch(f_t,x_t,1/delta,nperseg=2048)
+    plot.plot(f1,cohe)
+    plot.ylim([0,1.1])
 
-H1 = Sfx/Sff
-H1_noised = Sfx_noised/Sff
+    # frf_analytical = 1 / (k-(m*(2*np.pi*f)**2)+((2*np.pi*f)*c*1j))
 
-gama_fx_squared = np.abs(Sfx**2)/(Sff*Sxx)
-gama_fx_squared_noised = np.abs(Sfx_noised**2)/(Sff*Sxx_noised)
+    # plot.plot(w,np.abs(H1/tmax))
+    # plot.plot(w,np.abs(frf_analytical))
+    # plot.xlim([0,fs/2])
+    # plot.show()
 
-frf_analytical = 1 / (k-(m*(2*np.pi*w)**2)+((2*np.pi*w)*c*1j))
-
-# plot.plot(w,np.abs(H1/tmax))
-# plot.plot(w,np.abs(frf_analytical))
-# plot.xlim([0,fs/2])
-# plot.show()
-
-# plot.plot(w,np.unwrap(np.angle(H1/tmax)))
-# plot.plot(w,np.angle(frf_analytical))
-# plot.xlim([0,fs/2])
-# plot.show()
-
-plot.plot(w,np.abs(gama_fx_squared))
-plot.plot(w,np.abs(gama_fx_squared_noised))
-plot.ylim([0.9,1.1])
+    # plot.plot(w,np.unwrap(np.angle(H1/tmax)))
+    # plot.plot(w,np.angle(frf_analytical))
+    # plot.xlim([0,fs/2])
+    # plot.show()
 plot.show()
 
 
